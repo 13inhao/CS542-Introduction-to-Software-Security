30d5506541ddb1ce843c44d84ae24eb408e48e42
13inhao <47936184+13inhao@users.noreply.github.com>
2022-09-19 21:30:39 -0500

    .class update

--- Numeric Errors/src/NumericErrors.java
+++ Numeric Errors/src/NumericErrors.java
@@ -2,25 +2,27 @@ public class NumericErrors {
 
 	public static int add(int x, int y) throws ArithmeticException {
 		int sum = x + y;
+
+		// Case 1:
 		if (x >= 0 && y >= 0) {
 			if (sum < 0) {
 				throw new ArithmeticException("int overflow: add(" + x + ", " + y + ")");
 			}
 		}
-		
+
 		if (x <= 0 && y <= 0) {
 			if (sum > 0) {
 				throw new ArithmeticException("int overflow: add(" + x + ", " + y + ")");
 			}
 		}
-		
+
 		if ((x >= 0 && y <= 0) || (x <= 0 && y >= 0)) {
 			return sum;
 		}
-	
+
 		return sum;
 	}
-	
+
 	public static int subtract(int x, int y) throws ArithmeticException {
 		int diff = x - y;
 		if (x >= 0 && y <= 0) {
@@ -28,13 +30,13 @@ public class NumericErrors {
 				throw new ArithmeticException("int overflow: subtract(" + x + ", " + y + ")");
 			}
 		}
-		
+
 		if (x <= 0 && y >= 0) {
 			if (diff > 0) {
 				throw new ArithmeticException("int overflow: subtract(" + x + ", " + y + ")");
 			}
 		}
-		
+
 		if ((x >= 0 && y >= 0) || (x <= 0 && y <= 0)) {
 			return diff;
 		}
@@ -47,24 +49,24 @@ public class NumericErrors {
 				throw new ArithmeticException("int overflow: multiply(" + x + ", " + y + ")");
 			}
 		} else if (x < 0) {
-			if(x == -1 && y == Integer.MIN_VALUE) {
+			if (x == -1 && y == Integer.MIN_VALUE) {
 				throw new ArithmeticException("int overflow: multiply(" + x + ", " + y + ")");
 			}
-			
+
 			if (y < Integer.MAX_VALUE / x || y > Integer.MIN_VALUE / x) {
 				throw new ArithmeticException("int overflow: multiply(" + x + ", " + y + ")");
 			}
 		}
-		
-		return x*y;
+
+		return x * y;
 	}
-	
+
 	public static int divide(int x, int y) {
-		
+
 		return 0;
 	}
 
 	public static void main(String args[]) {
-		System.out.println(add(Integer.MAX_VALUE-1, Integer.MAX_VALUE-1));
+		System.out.println((Integer.MIN_VALUE+1) / (-1));
 	}
 }
